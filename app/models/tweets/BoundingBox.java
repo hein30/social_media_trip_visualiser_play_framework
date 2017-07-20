8bffd86897a716b82a423f206f329fd281e11b17
Hein Min Htike <htikeh@cardiff.ac.uk>
2017-07-20 11:35:56 +0100

    bounding box tests

--- app/models/tweets/BoundingBox.java
+++ app/models/tweets/BoundingBox.java
@@ -9,6 +9,8 @@ public class BoundingBox {
 
   private GeoLocation southWest;
   private GeoLocation northEast;
+  private GeoLocation southEast;
+  private GeoLocation northWest;
 
   public BoundingBox() {
     // for morphia
@@ -18,6 +20,8 @@ public class BoundingBox {
       double northEastLat) {
     this.southWest = new GeoLocation(southWestLat, southWestLong);
     this.northEast = new GeoLocation(northEastLat, northEastLong);
+    this.northWest = new GeoLocation(northEastLat, southWestLong);
+    this.southEast = new GeoLocation(southWestLat, northEastLong);
   }
 
   public boolean isLocationInBox(GeoLocation location) {
@@ -28,5 +32,37 @@ public class BoundingBox {
         && location.getLongitude() <= northEast.getLongitude();
 
     return isLatitudeInside && isLongitudeInside;
+  }
+
+  public GeoLocation getSouthWest() {
+    return southWest;
+  }
+
+  public void setSouthWest(GeoLocation southWest) {
+    this.southWest = southWest;
+  }
+
+  public GeoLocation getNorthEast() {
+    return northEast;
+  }
+
+  public void setNorthEast(GeoLocation northEast) {
+    this.northEast = northEast;
+  }
+
+  public GeoLocation getSouthEast() {
+    return southEast;
+  }
+
+  public void setSouthEast(GeoLocation southEast) {
+    this.southEast = southEast;
+  }
+
+  public GeoLocation getNorthWest() {
+    return northWest;
+  }
+
+  public void setNorthWest(GeoLocation northWest) {
+    this.northWest = northWest;
   }
 }
